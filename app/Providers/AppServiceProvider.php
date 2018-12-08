0934917419b5b85585dde0b9f605bc78b6dbcdd4
Tim O. Peters <timotewpeters@gmail.com>
2018-12-08 05:39:55 +0100

    boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,32 +15,33 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Dispatcher $events)
     {
-        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
-            $event->menu->add('MAIN NAVIGATION');
-            $event->menu->add([
-                'text' => 'Blog',
-                'url' => 'admin/blog',
-            ]);
-        });
+        // $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+        //     $event->menu->add(trans('menu.pages'));
+
+        //     $items = Page::all()->map(function (Page $page) {
+        //         return [
+        //             'text' => $page['title'],
+        //             'url' => route('admin.pages.edit', $page)
+        //         ];
+        //     });
+
+        //     $event->menu->add(...$items);
+        // });
     }
+    // public function boot(Dispatcher $events)
+    // {
+    //     $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+    //         $event->menu->add('MAIN NAVIGATION');
+    //         $event->menu->add([
+    //             'text' => 'Blog',
+    //             'url' => 'admin/blog',
+    //         ]);
+    //     });
+    // }
 
     /**
      *
-     *  public function boot(Dispatcher $events)
-    {
-        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
-            $event->menu->add(trans('menu.pages'));
-
-            $items = Page::all()->map(function (Page $page) {
-                return [
-                    'text' => $page['title'],
-                    'url' => route('admin.pages.edit', $page)
-                ];
-            });
-
-            $event->menu->add(...$items);
-        });
-    }
+     *
      */
     /**
      * Register any application services.
