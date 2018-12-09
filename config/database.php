9083bf9aac4e1f7d2a913ab300857d7714233b42
Tim O. Peters <timotewpeters@gmail.com>
2018-12-09 18:21:22 +0100

    mongodb atlas

--- config/database.php
+++ config/database.php
@@ -83,14 +83,17 @@ return [
         ],
         'mongodb' => [
             'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', 'localhost'), // multiple ['server1', 'server2']
-            'port'     => env('DB_PORT', 27017),
+            'dsn' => env('DB_DSN'),
             'database' => env('DB_DATABASE'),
+            // 'driver'   => 'mongodb',
+            // 'host'     => env('DB_HOST', 'localhost'), // multiple ['server1', 'server2']
+            // 'port'     => env('DB_PORT', 27017),
+            // 'database' => env('DB_DATABASE'),
             // 'username' => env('DB_USERNAME'),
             // 'password' => env('DB_PASSWORD'),
-            'options'  => [
-                'database' => 'admin' // sets the authentication database required by mongo 3
-            ]
+            // 'options'  => [
+            //     'database' => 'admin' // sets the authentication database required by mongo 3
+            // ]
         ]
 
     ],
